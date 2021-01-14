37fcfb3e327cfd3133da8b16449415de6f2c38ff
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-01-13 19:50:13 -0800

    updated html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,16 +4,16 @@ const path = require("path");
 module.exports = (app) => {
     // Route to home page
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/html/index.html"));
     });
 
     // Exercise page
     app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+        res.sendFile(path.join(__dirname, "../public/html/exercise.html"));
     });
 
     // Stats page
     app.get("/stats", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/stats.html"));
+        res.sendFile(path.join(__dirname, "../public/html/stats.html"));
     });
 }
