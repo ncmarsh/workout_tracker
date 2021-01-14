22b9bb5d880d74e58e4e76020f67a88991ae5347
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2021-01-13 19:50:55 -0800

    updated get api call for workout range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,71 @@
+const db = require("../models");
+
 // API routes
 module.exports = (app) => {
-    // getLastWorkout
+    // getLastWorkout - retrieve
     app.get("/api/workouts", (req, res) => {
 
     });
 
-    // addExercise
+    // addExercise - update
     app.put("/api/workouts/:id", (req, res) => {
 
     });
 
-    // createWorkout
+    // createWorkout - create
     app.post("/api/workouts", (req, res) => {
-
-    })
-
-    // getWorkoutsInRange
-    app.get("/api/workouts/range", (req, res) => {
-
+        
     });
-}
+
+    // getWorkoutsInRange - retrieve
+    // app.get("/api/workouts/range", (req, res) => {
+    //     db.Workout.find({}, (err, dbWorkout) => {
+    //         if (err) {
+    //             console.log(err);
+    //             res.send(err);
+    //         } else {
+    //             // console.log(dbWorkout);
+    //             console.log(dbWorkout[0].exercises);
+    //             console.log(dbWorkout[1].exercises);
+    //             console.log(dbWorkout[2].exercises);
+    //             console.log(dbWorkout[3].exercises);
+    //             console.log(dbWorkout[4].exercises);
+    //             console.log(dbWorkout[5].exercises);
+    //             console.log(dbWorkout[6].exercises);
+    //             console.log(dbWorkout[7].exercises);
+    //             console.log(dbWorkout[8].exercises);
+    //             res.json(dbWorkout);
+    //         }
+    //     });
+    // });
+    // app.get("/api/workouts/range", (req, res) => {
+    //     db.Workout.find({})
+    //         .then(dbWorkout => {
+    //             console.log(dbWorkout);
+    //             res.json(dbWorkout);
+    //         })
+    //         .catch(err => {
+    //             res.json(err);
+    //         });
+    // });
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.aggregate([
+                {
+                    $addFields: {
+                        totalDuration: { $sum: "$exercises.duration" },
+                        totalPounds: { $sum: "$exercises.pounds" }
+                    }
+                },
+                {
+                    $limit: 7
+                }
+            ])
+            .then(dbWorkout => {
+                console.log(dbWorkout);
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+};
